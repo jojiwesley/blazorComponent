3e68e6f209b4cec9c1a8a7accc5d98d84ee597e3
Joji W. <joji.wesley@hotmail.com>
2024-09-13 16:34:11 -0300

    Ajustes de layout do banner

---
+++ BlazorComponents/Components/Layout/MainLayout.razor.js
@@ -0,0 +1,28 @@
+﻿export function onLoad() {
+    /*addFormListeners();
+    registerCookieConsent();
+    maskFields();
+
+    window.addEventListener('scroll', removeDivOnScroll);
+
+    AOS.init({
+        once: true,
+        duration: 1000,
+    });*/
+}
+
+export function onUpdate() {
+   /* addFormListeners();
+    refreshReCaptchaToken();
+    maskFields();
+    AOS.init({
+        once: true,
+        duration: 1000,
+    });*/
+    document.querySelectorAll("link[media='print']")?.forEach(e => { e.media = 'all' });
+    /*window.addEventListener('scroll', removeDivOnScroll);*/
+}
+
+export function onDispose() {
+    //console.log('Disposed');
+}
---
+++ BlazorComponents/Components/Pages/BannersComponents.razor.js
@@ -0,0 +1,31 @@
+﻿export function onLoad() {
+    const swiper = new Swiper('.swiper__bannerSlider', {
+        // Optional parameter   s
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+}
+
+export function onUpdate() {
+  
+}
+
+export function onDispose() {
+   
+}
